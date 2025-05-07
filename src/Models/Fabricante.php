347304e4d949571c9ae668605f97746f95be52d4
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-05-07 09:01:20 -0300

    Adicionando setters e getters

--- src/Models/Fabricante.php
+++ src/Models/Fabricante.php
@@ -2,5 +2,20 @@
 namespace ExemploCrud;
 
 final class Fabricante {
-    
+    private ?int $id; // null | int
+    private string $nome;
+
+    public function __construct(string $nome, ?int $id = null)
+    {
+        $this->setNome($nome);
+        $this->setId($id);
+        $this->validar();
+    }
+
+    private function validar():void
+    {
+        if(empty($this->nome)){
+            throw new InvalidArgumentException("Nome é obrigatório");
+        }
+    }
 }
