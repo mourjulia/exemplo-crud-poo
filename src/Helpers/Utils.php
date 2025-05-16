6d4a615f67015912906aead60b27266baff4a069
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-05-16 10:36:01 -0300

    metodo e arquivo para registro de log

--- src/Helpers/Utils.php
+++ src/Helpers/Utils.php
@@ -21,4 +21,17 @@ final class Utils
         $precoFormatado = "R$ " . number_format($valor, 2, ",", ".");
         return $precoFormatado;
     }
+
+    public static function registrarLog(Throwable $e): void
+    {
+        date_default_timezone_set('America/Sao_Paulo');
+
+        $mensagem = "[" . date("Y-m-d H:i:s") . "]\n" .
+            "Arquivo: " . $e->getFile() . "\n" .
+            "Linha: " . $e->getLine() . "\n" .
+            "Mensagem: " . $e->getMessage() . "\n\n";
+
+
+        file_put_contents(__DIR__ . '/../../logs/erros.log', $mensagem, FILE_APPEND);
+    }
 }
